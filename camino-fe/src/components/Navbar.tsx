1b67141f55d5a63401959db5ac8b6498d8c0402c
Can Gökçeaslan <can.gokceaslan@petour.com>
2024-11-14 03:18:52 +0300

    navbar fixes

--- camino-fe/src/components/Navbar.tsx
+++ camino-fe/src/components/Navbar.tsx
@@ -1,38 +1,46 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 import { Menubar } from 'primereact/menubar';
 import { Button } from 'primereact/button';
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, useLocation } from 'react-router-dom';
 import { useWallet } from '../contexts/WalletContext';
 import { MenuItem } from 'primereact/menuitem';
 
 import geeseIcon from '../assets/geese-icon.png';
+import '../assets/Navbar.css';
 
 export const Navbar: React.FC = () => {
   const navigate = useNavigate();
+  const location = useLocation();
   const { walletState, connectWallet, disconnectWallet } = useWallet();
 
-  const items: MenuItem[] = [
-    {
-      label: 'Wild Geese',
-      icon: <img src={geeseIcon} alt="Home Icon" style={{ width: '40px', height: '40px', marginRight: '10px' }} />,
-      command: () => navigate('/'),
-    },
-    {
-      label: 'Home',
-      icon: 'pi pi-home',
-      command: () => navigate('/'),
-    },
-    {
-      label: 'Management',
-      icon: 'pi pi-cog',
-      command: () => navigate('/management'),
-    },
-    {
-      label: 'Business',
-      icon: 'pi pi-briefcase',
-      command: () => navigate('/business'),
-    },
-  ];
+  const items = useMemo<MenuItem[]>(() => {
+    return [
+      {
+        label: 'Wild Geese',
+        icon: <img src={geeseIcon} alt="Home Icon" style={{ width: '40px', height: '40px', marginRight: '10px' }} />,
+        command: () => navigate('/'),
+        className: location.pathname === '/' ? 'active' : '',
+      },
+      {
+        label: 'Home',
+        icon: 'pi pi-home',
+        command: () => navigate('/'),
+        className: location.pathname === '/' ? 'active' : '',
+      },
+      {
+        label: 'Management',
+        icon: 'pi pi-cog',
+        command: () => navigate('/management'),
+        className: location.pathname === '/management' ? 'active' : '',
+      },
+      {
+        label: 'Business',
+        icon: 'pi pi-briefcase',
+        command: () => navigate('/business'),
+        className: location.pathname === '/business' ? 'active' : '',
+      },
+    ];
+  }, [location, navigate]);
 
   const end = (
     <Button
